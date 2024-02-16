5e32bd3a1d21dcea1f6a6594d698c2bc7017599c
Rushi Patel <201801166@daiict.ac.in>
2024-02-16 16:53:20 -0700

    Added Web Tier

---
+++ web-tier.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import csv
+import os
+
+app = Flask(__name__)
+lookup_path = 'Classification Results on Face Dataset (1000 images).csv'
+lookup = {}
+
+def readCsv():
+    global lookup
+    with open(lookup_path, 'r') as csvFile:
+        csv_reader = csv.reader(csvFile)
+        lookup = {row[0]: row[1] for row in csv_reader}
+
+readCsv()
+
+@app.route('/', methods=['POST'])
+def classifyImage():
+    print("Received request")
+
+    try:
+        inputFile = request.files['inputFile']
+        filename = inputFile.filename
+        baseFilename = os.path.splitext(filename)[0]
+
+        label = lookup.get(baseFilename)
+        
+        if label is not None:
+            data = baseFilename+':'+label
+            return data, 200
+        else:
+            return jsonify({'error': f"Filename {baseFilename} not found in lookup"}), 404
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8888)))
+
